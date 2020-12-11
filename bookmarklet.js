cbacab9edd72da80f022d1e210d43fbe0b5262fa
kota-a <53009463+kota-a@users.noreply.github.com>
2020-12-12 03:01:05 +0900

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,14 @@ javascript:(function(url){
   request.responseType = 'text';
   
   request.onload = function () {
-    eval(this.response);
+    // If the response is javascript code, use the following code.
+    // eval(this.response);
+    
+    // If the response is html, use the following code.
+    // (In the case of the execution javascript code is embedded in first 'pre' tag)
+    var page = document.createElement('html');
+    page.innerHTML = this.response;
+    eval(page.getElementsByTagName('pre')[0].innerHTML);
   };
   
   request.send();
